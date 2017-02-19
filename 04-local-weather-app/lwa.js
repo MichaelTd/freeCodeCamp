1b8abe46f9f72eef1be25670a094ba1ccc54ae72
MichaelTd <tsouchlarakis@gmail.com>
2017-02-19 21:11:52 +0200

    Add files via upload

---
+++ 04-local-weather-app/lwa.js
@@ -0,0 +1,42 @@
+var far;
+var cel;
+
+function loadWeather(location, woeid){
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'C',
+    success: function(weather){
+      cel = weather.temp + '&deg; C';
+      far = weather.alt.temp + '&deg; F';
+      $(".image").attr("src", weather.image);
+      $(".location").html(weather.city);
+      $(".temperature").html(cel);
+      $(".windspeed").html(weather.wind.speed + '&nbsp;' + weather.units.speed);
+      $(".humidity").html(weather.humidity+' %');
+    },
+    error: function(error){
+      $(".error-title").html('<p>Weather API Error:</p>');
+      $(".error").html('<p>' + error + '</p>');
+    }
+  });
+}
+
+function togglestyle(el){
+  if ($(el).hasClass("C")) {
+    $(".temperature").html(far);
+    $(el).toggleClass("F").toggleClass("C").attr("value", "to Celsius");
+  } else {
+    $(".temperature").html(cel);
+    $(el).toggleClass("F").toggleClass("C").attr("value", "to Fahrenheit");
+  }
+}
+
+$.getJSON("http://ip-api.com/json",function(){})
+.done(function(data) {
+  loadWeather(data.lat + ',' + data.lon);
+})
+.fail(function(error){
+  $(".error-title").html('<p>jQuery Error:</p>');
+  $(".error").html('<p>' + error + '</p>');
+})
