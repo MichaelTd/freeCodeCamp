033af2be32299f34ba26f213008b86c45f9ca1c6
MichaelTd <tsouchlarakis@gmail.com>
2017-02-24 18:07:10 +0200

    Update lwa.js

--- 04-local-weather-app/lwa.js
+++ 04-local-weather-app/lwa.js
@@ -25,10 +25,10 @@ function loadWeather(location, woeid){
 function togglestyle(el){
   if ($(el).hasClass("C")) {
     $(".temperature").html(far);
-    $(el).toggleClass("F").toggleClass("C").attr("value", "Celsius");
+    $(el).toggleClass("F C").attr("value", "Celsius");
   } else {
     $(".temperature").html(cel);
-    $(el).toggleClass("F").toggleClass("C").attr("value", "Fahrenheit");
+    $(el).toggleClass("F C").attr("value", "Fahrenheit");
   }
 }
 
