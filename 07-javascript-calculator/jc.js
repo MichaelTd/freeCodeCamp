b8f1b088cf6bfc44a09851755e59500d1a06b75a
MichaelTd <tsouchlarakis@gmail.com>
2017-03-19 21:28:12 +0200

    Update jc.js

--- 07-javascript-calculator/jc.js
+++ 07-javascript-calculator/jc.js
@@ -2,7 +2,11 @@ function updateDisplay(param){
   switch (param) {
     case "=": jsCalc.display.value = eval(jsCalc.display.value);
       break;
-    case "CL": jsCalc.display.value = "";
+    case "C": jsCalc.display.value = "";
+      break;
+    case "+/-": jsCalc.display.value = eval(jsCalc.display.value) * -1;
+      break;
+    case "BS": jsCalc.display.value = jsCalc.display.value.substring(0, jsCalc.display.value.length - 1);
       break;
     default: jsCalc.display.value += param;
   }
