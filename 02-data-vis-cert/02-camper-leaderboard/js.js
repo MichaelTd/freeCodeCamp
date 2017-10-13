7e82919cb75e1bb92b746f285fdb6b840c73cca7
MichaelTd <tsouchlarakis@gmail.com>
2017-10-13 17:26:28 +0300

    camper-leaderboard

--- 02-data-vis-cert/02-camper-leaderboard/js.js
+++ 02-data-vis-cert/02-camper-leaderboard/js.js
@@ -1,10 +1,30 @@
-'use strict';
+"use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+	if (!(instance instanceof Constructor)) {
+		throw new TypeError("Cannot call a class as a function");
+	}
+}
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _possibleConstructorReturn(self, call) {
+	if (!self) {
+		throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+	}
+	return call && (typeof call === "object" || typeof call === "function") ? call : self;
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) {
+	if (typeof superClass !== "function" && superClass !== null) {
+		throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+	}
+
+	subClass.prototype = Object.create(superClass && superClass.prototype, {
+		constructor: { value: subClass, enumerable: false, writable: true, configurable: true }
+	});
+
+	if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+
+}
 
 var Table = function (_React$Component) {
 	_inherits(Table, _React$Component);
@@ -34,7 +54,8 @@ var Table = function (_React$Component) {
 				),
 				React.createElement(
 					'a',
-					{ href: 'https://www.freecodecamp.com/' + item.username, target: '_blank' },
+					//{ href: 'https://www.freecodecamp.com/' + item.username, target: '_blank' },
+					{ href: 'https://www.freecodecamp.com/' + item.username },
 					React.createElement('img', { src: item.img }),
 					React.createElement(
 						'span',
@@ -59,7 +80,8 @@ var Table = function (_React$Component) {
 				),
 				React.createElement(
 					'a',
-					{ href: 'https://www.freecodecamp.com/' + item.username, target: '_blank' },
+					//{ href: 'https://www.freecodecamp.com/' + item.username, target: '_blank' },
+					{ href: 'https://www.freecodecamp.com/' + item.username },
 					React.createElement('img', { src: item.img }),
 					React.createElement(
 						'span',
