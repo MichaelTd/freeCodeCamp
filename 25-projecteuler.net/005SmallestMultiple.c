767df4b7d0551d56fb1478976ec84272a16a95b9
MichaelTd <tsouchlarakis@gmail.com>
2019-02-27 06:54:40 +0200

    ProjectEuler

--- 25-projecteuler.net/005SmallestMultiple.c
+++ 25-projecteuler.net/005SmallestMultiple.c
@@ -9,8 +9,17 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 #include <stdio.h>
 
-int main(void)
-{
-  printf("\n");
-  return (0);
+int main(void) {
+
+    int i = 1;
+
+    while (i %  2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 || i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 || i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 || i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 || i % 18 != 0 || i % 19 != 0 || i % 20 != 0 ) {
+
+        i++;
+
+    }
+
+    printf("%d\n", i);
+
+    return (0);
 }
